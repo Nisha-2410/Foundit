b13ba7bd35a32936e0e8beccf8dbc5330d471d87
nisha <nishamahato949@gmail.com>
2025-07-07 22:41:10 +0530

    made some corrections
    put gitignore in forntend

---
+++ Foundit_backend/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import searchRoutes from "./routes/search.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/search", searchRoutes);
+
+app.get("/", (req, res) => {
+  res.send("FoundIt backend is live 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
