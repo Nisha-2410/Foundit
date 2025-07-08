e802bb9466df4f62b5a3caeb8ede01f09ff3fe48
nisha <nishamahato949@gmail.com>
2025-07-08 13:28:29 +0530

    updated the frontend

--- Foundit_frontend/src/App.jsx
+++ Foundit_frontend/src/App.jsx
@@ -1,15 +1,19 @@
-import Hero from "./components/Hero"
-import Navbar from "./components/Navbar"
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import ResultsPage from "./pages/ResultsPage"; // We'll create this
 
 function App() {
-
   return (
-    <>
-    <Navbar/>
-    <Hero/>
-    </>
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/results" element={<ResultsPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
+
